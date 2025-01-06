f3201a173b4f2634f0639c9fe9b0b6d6667f3f9d
KOUSHIK PINNU <pinnukoushik1@gmail.com>
2025-01-07 00:03:33 +0530

    Fixed Acomodation Link

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/events" element={<EventPage />} />
-      <Route path="/accommodation" element={<AccommodationPage />} />
+      <Route path="/acomodation" element={<AccommodationPage />} />
     </Routes>
   </>
   );
