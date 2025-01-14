c84350696324b62fa869bed56f6be6e26748bdb9
Ns-AnoNymouS <lankotunaveen@gmail.com>
2025-01-14 22:29:45 +0530

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,44 +1,71 @@
 function Footer() {
   return (
-    <footer className="bg-[#0f0f0f] text-[#f5e0a9] py-6">
-      <div className="container mx-auto px-6 flex flex-col items-center justify-between md:flex-row">
-        {/* Left Section */}
-        <div className="flex flex-col items-center md:items-start">
-          <p className="text-sm lg:text-base font-serif">
-            © 2025 IIITS Abhisarga. All rights reserved.
-          </p>
-          <p className="text-xs lg:text-sm font-serif mt-2">
-            Designed with ❤️ by IIITS Team
-          </p>
-        </div>
+    <footer className="bg-[#0f0f0f] text-[#f5e0a9] flex flex-wrap justify-between items-end">
+      {/* Left Section */}
+      <div className="left">
+        <img
+          src="./assets/corner-bottom-right.png"
+          alt="Decorative Corner"
+          className="transform scale-x-[-1] w-[70px] md:w-[100px]"
+        />
+      </div>
 
-        {/* Right Section */}
-        <div className="flex space-x-4 mt-4 md:mt-0">
-          <a
-            href="https://www.facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-[#f5e0a9] hover:text-white transition duration-200"
-          >
-            Facebook
+      {/* Center Section */}
+      <div className="center flex flex-col items-center text-center">
+        <img
+          src="./full_logo_2.png"
+          alt="Full Logo"
+          className="h-12 md:h-20"
+        />
+        <div className="social-media flex space-x-4 mt-4">
+          {/* Social Media Icons */}
+          <a href="#" aria-label="Instagram">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 md:h-5 md:w-5"
+              fill="#AE8E48"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+            </svg>
           </a>
-          <a
-            href="https://www.instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-[#f5e0a9] hover:text-white transition duration-200"
-          >
-            Instagram
+          <a href="#" aria-label="LinkedIn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 md:h-5 md:w-5"
+              fill="#AE8E48"
+              viewBox="0 0 24 24"
+            >
+              <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+            </svg>
           </a>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-[#f5e0a9] hover:text-white transition duration-200"
-          >
-            Twitter
+          <a href="#" aria-label="YouTube">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 md:h-5 md:w-5"
+              fill="#AE8E48"
+              viewBox="0 0 24 24"
+            >
+              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
+            </svg>
           </a>
         </div>
+        <img src="./divider.png" alt="Divider" className="h-4 md:h-5 my-4" />
+        <p
+          className="text-sm md:text-base font-serif"
+          style={{ fontFamily: "MedievalSharp, serif", color: "#AE8E48" }}
+        >
+          Designed with ❤️ by IIITS Team
+        </p>
+      </div>
+
+      {/* Right Section */}
+      <div className="right">
+        <img
+          src="./assets/corner-bottom-right.png"
+          alt="Decorative Corner"
+          className="w-[70px] md:w-[100px]"
+        />
       </div>
     </footer>
   );
